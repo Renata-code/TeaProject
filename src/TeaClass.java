248ca8b8344a1cde9ffaca99c8600d747fc47daa
renatasaidjanova <otanazarov@yahoo.com>
2020-10-04 20:05:08 -0400

    coffee

--- src/TeaClass.java
+++ src/TeaClass.java
@@ -3,6 +3,8 @@ public class TeaClass {
     public static void main(String[] args) {
 
         System.out.println("Tea with milk");
+
+        System.out.println("Let's drink coffee");
         
     }
 }
